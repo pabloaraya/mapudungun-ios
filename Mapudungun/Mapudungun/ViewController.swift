cfaaae26f15909d27a01e42e162ab959b9bd4dcc
Camila Eyzaguirre <ceyzaguirrev@gmail.com>
2015-09-10 02:22:34 -0300

    Podfile, lectura de message según array, uso de SwiftyJson, mejoras generales del código.

--- Mapudungun/Mapudungun/ViewController.swift
+++ Mapudungun/Mapudungun/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SwiftyJSON
 
 class ViewController: UIViewController {
 
@@ -46,29 +47,49 @@ class ViewController: UIViewController {
     func call(word: NSString){
         //validar primero
         if ((from==PARAM_SPANISH || from==PARAM_MAPUDUNGUN) && (to==PARAM_SPANISH || to==PARAM_MAPUDUNGUN) && word != "" ){
+            
             //hago la consulta al servidor
-            let url = NSURL(string: "http://mapudungun.org/api?from=\(from)&to=\(to)&word=\(word)")
-            let task = NSURLSession.sharedSession().dataTaskWithURL(url!) {(data, response, error) in
-                //Leo el retorno
-                if (!self.validateIfString(data)){
-                    let result = self.parseJSON(data)
-                    //Pregunto si la respuesta es statusCode 200 para mostrar resultado
-                    if (result["statusCode"] as! Int == 200){
+            let original = "http://mapudungun.org/api?from=\(from)&to=\(to)&word=\(word)"
+            let urlString = original.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLFragmentAllowedCharacterSet())
+            let url = NSURL(string: urlString!)!
+            
+            let task = NSURLSession.sharedSession().dataTaskWithURL(url) {(data, response, error) in
+                
+                let result = JSON(data: data)
+                //Pregunto si la respuesta es statusCode 200 para mostrar resultado
+                switch(result["statusCode"]){
+                    case 200:
+                        if(result["status"] == "success"){
+                            //Show array result
+                            var message = ""
+                            for (key: String, subJson: JSON) in result["message"] {
+                                if (message == ""){
+                                    message = message + subJson.stringValue
+                                }else{
+                                    message = message + ", " + subJson.stringValue
+                                }
+                            }
+                            dispatch_async(dispatch_get_main_queue(), {
+                                self.lblResult.text = message
+                            });
+                        }else{
+                            //Word maybe not found
+                            dispatch_async(dispatch_get_main_queue(), {
+                                self.lblResult.text = result["message"].stringValue
+                            });
+                        }
+                    break;
+                    default:
+                        //StatusError (server)
                         dispatch_async(dispatch_get_main_queue(), {
-                            self.lblResult.text = (result["message"] as! String)
+                            self.lblResult.text = result["message"].stringValue
                         });
-                    }
+                        break;
                 }
             }
             task.resume()
         }
-    }
-    
-    func validateIfString(data: AnyObject) -> Bool {
-        if (data as? [String] != nil) {
-            return true
-        }
-        return false
+
     }
     
     func parseJSON(inputData: NSData) -> NSDictionary{
